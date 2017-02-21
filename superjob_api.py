74c739ae1ae15d496001fa59072c921437306f35
Платонов Александр <alliotalp@yandex.ru>
2017-02-21 23:49:13 +0300

    api key is read only once

--- superjob_api.py
+++ superjob_api.py
@@ -1,8 +1,12 @@
 import requests
 import os
 
+
+superjob_api_key = os.environ.get('SUPERJOB_API_KEY')
+
+
 def make_superjob_request(relative_url, url_params=None):
-    headers = {'X-Api-App-Id' : os.environ.get('SUPERJOB_API_KEY')}
+    headers = {'X-Api-App-Id' : superjob_api_key}
     response = requests.get('https://api.superjob.ru/2.0/%s' % relative_url,
                             params=url_params, headers=headers)
     return response.json()
