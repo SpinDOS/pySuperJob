39adfb18d2d49dc3e6aae78d28346e36b5566648
Alexander Platonov <alexanderplat757@gmail.com>
2017-02-27 18:21:52 +0300

    Rename global variable

--- superjob_api.py
+++ superjob_api.py
@@ -1,12 +1,13 @@
 import requests
 import os
 
+__all__= ['make_superjob_request']
 
-superjob_api_key = os.environ.get('SUPERJOB_API_KEY')
+SUPERJOB_API_KEY = os.environ.get('SUPERJOB_API_KEY')
 
 
 def make_superjob_request(relative_url, url_params=None):
-    headers = {'X-Api-App-Id' : superjob_api_key}
+    headers = {'X-Api-App-Id' : SUPERJOB_API_KEY}
     response = requests.get('https://api.superjob.ru/2.0/%s' % relative_url,
                             params=url_params, headers=headers)
     return response.json()
